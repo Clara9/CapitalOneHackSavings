c8356aa3d050d1d5058785036c7381fb76f3c2c0
xz33 <xz33@illinois.edu>
2019-08-08 21:08:52 -0400

    Set up confirmation page

--- app/src/main/java/com/example/seshack/ConfirmationActivity.java
+++ app/src/main/java/com/example/seshack/ConfirmationActivity.java
@@ -1,7 +1,12 @@
 package com.example.seshack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ConfirmationActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        Intent intent = getIntent();
+        TextView moneySaved = (TextView) findViewById(R.id.moneySaved);
+        TextView category = (TextView) findViewById(R.id.category);
+        category.setText("On " + intent.getStringExtra("SAVING_CATEGORY"));
+        moneySaved.setText("$ " + intent.getStringExtra("AMOUNT_SAVED"));
     }
 }
--- app/src/main/java/com/example/seshack/PriceActivity.java
+++ app/src/main/java/com/example/seshack/PriceActivity.java
@@ -3,19 +3,51 @@ package com.example.seshack;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class PriceActivity extends AppCompatActivity {
+    private EditText priceInput;
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price);
 
+        priceInput = (EditText) findViewById(R.id.priceInput);
+        confirmButton = (Button) findViewById(R.id.confirm);
+
         configureBackButton();
-        testIntent();
+        configureConfirmButton();
+        configurePriceInput();
+    }
+
+    private void configurePriceInput() {
+        priceInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!priceInput.getText().toString().isEmpty()) {
+                    confirmButton.setEnabled(true);
+                } else {
+                    confirmButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     private void configureBackButton() {
@@ -28,13 +60,18 @@ public class PriceActivity extends AppCompatActivity {
         });
     }
 
-    private void testIntent() {
-        TextView category = (TextView) findViewById(R.id.category);
-        Intent intent = getIntent();
-        if (intent != null) {
-            String data = intent.getStringExtra("SAVING_CATEGORY");
-            category.setText(String.valueOf(data));
-        }
+    private void configureConfirmButton() {
+        confirmButton.setEnabled(false);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PriceActivity.this, ConfirmationActivity.class);
+                intent.putExtra("SAVING_CATEGORY", getIntent().getStringExtra("SAVING_CATEGORY"));
+                intent.putExtra("AMOUNT_SAVED", priceInput.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
+
 
 }
