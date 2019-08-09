5fe677f542b071043bbd5e232f034ae3d5911287
Tiffany Zhong <tzhong00@gmail.com>
2019-08-09 06:05:28 -0400

    Finished allowing user to select charity of their own category

--- app/src/main/java/com/example/charityselection/MainActivity.java
+++ app/src/main/java/com/example/charityselection/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -32,6 +34,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     public List<CharitiesItem> allcharities;
+    public String selectedItemText="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 .connectTimeout(5000, TimeUnit.SECONDS)
                 .addInterceptor(logging)
                 .build();
-        Retrofit builder=new Retrofit.Builder()
+        final Retrofit builder=new Retrofit.Builder()
                 .baseUrl("https://data.orghunter.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(httpClient)
@@ -65,29 +69,45 @@ public class MainActivity extends AppCompatActivity {
         listView=(ListView)findViewById(R.id.paginationList);
         final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(aa);
-        //Calling the API
-        CharityClient client = builder.create(CharityClient.class);
-        Call<CharityResponse> call= client.charitiesForCategory("5ccf3fdb30b8bea29185a75b07530d03","education","5");
+        String selected = spinner.getSelectedItem().toString();
+        Toast.makeText(getApplicationContext(),selected,Toast.LENGTH_SHORT).show();
 
-        call.enqueue(new Callback<CharityResponse>() {
-            public String charName;
+        //String text = spinner.getSelectedItem().toString();
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onResponse(Call<CharityResponse> call, Response<CharityResponse> response) {
-                CharityResponse response1 = response.body();
-                List<DataItem> charities= response1.getData();
-                for (DataItem dataitem:charities) {
-                    charName=dataitem.getCharityName();
-                    aa.add(charName);
-                }
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItemText = (String) adapterView.getItemAtPosition(i);
+                Toast.makeText
+                        (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                        .show();
+                //Calling the API
+                CharityClient client = builder.create(CharityClient.class);
 
+                Call<CharityResponse> call= client.charitiesForCategory("5ccf3fdb30b8bea29185a75b07530d03",selectedItemText,"5");
+                call.enqueue(new Callback<CharityResponse>() {
+                    public String charName;
+                    @Override
+                    public void onResponse(Call<CharityResponse> call, Response<CharityResponse> response) {
+                        CharityResponse response1 = response.body();
+                        List<DataItem> charities= response1.getData();
+                        aa.clear();
+                        for (DataItem dataitem:charities) {
+                            charName=dataitem.getCharityName();
+                            aa.add(charName);
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<CharityResponse> call, Throwable t) {
+                        Toast.makeText(MainActivity.this,"Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
-
-
             @Override
-            public void onFailure(Call<CharityResponse> call, Throwable t) {
-                Toast.makeText(MainActivity.this,"Error!", Toast.LENGTH_SHORT).show();
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+
     }
 }
